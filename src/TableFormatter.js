060a98fc38460914214c09322d17ae92b5783fcd
Disnel Rodriguez Rodriguez <disnel.rodriguez@axelerant.com>
2021-05-31 15:13:20 -0400

    Added ETA to table view

--- src/TableFormatter.js
+++ src/TableFormatter.js
@@ -9,6 +9,17 @@ import {
   Text,
 } from "@chakra-ui/react";
 
+const getETA = (date, start = 19, end = 22) => {
+  const dateObj = new Date(date);
+  const startDate = new Date(Number(dateObj));
+  const endDate = new Date(Number(dateObj));
+  startDate.setDate(dateObj.getDate() + start);
+  endDate.setDate(dateObj.getDate() + end);
+  return `${startDate.toLocaleString([], {
+    dateStyle: "short",
+  })} - ${endDate.toLocaleString([], { dateStyle: "short" })}`;
+};
+
 const TableFormatter = ({ elements }) => {
   return (
     <Table variant='striped' colorScheme='teal'>
@@ -19,6 +30,7 @@ const TableFormatter = ({ elements }) => {
           <Th>Track Number</Th>
           <Th>Status</Th>
           <Th>Dep. date</Th>
+          <Th>ETA</Th>
           <Th>Mail guide</Th>
         </Tr>
       </Thead>
@@ -34,6 +46,7 @@ const TableFormatter = ({ elements }) => {
                 </Text>
               </Td>
               <Td>{parcel.depdate ? parcel.depdate : "no date"}</Td>
+              <Td>{parcel.depdate ? getETA(parcel.depdate) : "no date"}</Td>
               <Td>{parcel.mailguide ? parcel.mailguide : "no mail guide"}</Td>
             </Tr>
           );
