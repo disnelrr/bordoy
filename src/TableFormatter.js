ee0641b0cb5503eeb64bef03cd590026c3118a06
Disnel Rodriguez Rodriguez <disnel.rodriguez@axelerant.com>
2021-06-09 08:16:14 -0400

    Added style to ETA

--- src/TableFormatter.js
+++ src/TableFormatter.js
@@ -20,6 +20,26 @@ const getETA = (date, start = 19, end = 22) => {
   })} - ${endDate.toLocaleString([], { dateStyle: "short" })}`;
 };
 
+const isInETA = (date, start = 18, end = 23) => {
+  const dateObj = new Date(date);
+  const today = new Date();
+  const startDate = new Date(Number(dateObj));
+  const endDate = new Date(Number(dateObj));
+  startDate.setDate(dateObj.getDate() + start);
+  endDate.setDate(dateObj.getDate() + end);
+  return (
+    startDate.getTime() < today.getTime() && today.getTime() < endDate.getTime()
+  );
+};
+
+const isAfterETA = (date, end = 22) => {
+  const dateObj = new Date(date);
+  const today = new Date();
+  const endDate = new Date(Number(dateObj));
+  endDate.setDate(dateObj.getDate() + end);
+  return today.getTime() > endDate.getTime();
+};
+
 const TableFormatter = ({ elements }) => {
   return (
     <Table variant='striped' colorScheme='teal'>
@@ -36,22 +56,35 @@ const TableFormatter = ({ elements }) => {
       </Thead>
       <Tbody>
         {elements.map((parcel, index) => {
+          let styleForETA = { fontWeight: "bold" };
+          if (parcel.depdate && isInETA(parcel.depdate)) {
+            styleForETA = { color: "red", ...styleForETA };
+          } else if (isAfterETA(parcel.depdate)) {
+            styleForETA = { color: "green", ...styleForETA };
+          } else {
+            styleForETA = {};
+          }
           return (
             <Tr key={index}>
               <Td>{parcel.noorder}</Td>
               <Td>{parcel.hbl}</Td>
               <Td>
                 <Text
-                  color={parcel.status === "EN CONSIGNATARIO" ? "green" : null}
-                  fontWeight={
-                    parcel.status === "EN CONSIGNATARIO" ? "bold" : null
+                  style={
+                    parcel.status === "EN CONSIGNATARIO"
+                      ? { color: "green", fontWeight: "bold" }
+                      : {}
                   }
                 >
                   {parcel.status}
                 </Text>
               </Td>
               <Td>{parcel.depdate ? parcel.depdate : "no date"}</Td>
-              <Td>{parcel.depdate ? getETA(parcel.depdate) : "no date"}</Td>
+              <Td>
+                <Text style={styleForETA}>
+                  {parcel.depdate ? getETA(parcel.depdate) : "no date"}
+                </Text>
+              </Td>
               <Td>{parcel.mailguide ? parcel.mailguide : "no mail guide"}</Td>
             </Tr>
           );
